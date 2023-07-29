9094a24fe1063c3b2a4240d5620408e7a7322e2e
ShinHeeJin <sisesu92@gmail.com>
2023-07-29 16:27:14 +0900

    fix Variable

--- src/dev/heejin/Main.java
+++ src/dev/heejin/Main.java
@@ -1,8 +1,10 @@
 package dev.heejin;
 
+import dev.heejin.ch02.Variable;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("java");
+        new Variable().main();
     }
 }
---
+++ src/dev/heejin/ch02/Variable.java
@@ -0,0 +1,9 @@
+package dev.heejin.ch02;
+
+public class Variable {
+    public void main(){
+        int a = 1;
+        a = 3;
+        System.out.println("a = " + a);
+    }
+}
