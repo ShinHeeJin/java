d8ad994513e924e94c44f78930725320cc72228c
ShinHeeJin <sisesu92@gmail.com>
2023-07-29 20:37:07 +0900

    ch04 MathTest

--- src/dev/heejin/Main.java
+++ src/dev/heejin/Main.java
@@ -1,10 +1,10 @@
 package dev.heejin;
 
-import dev.heejin.ch03.Practice01;
+import dev.heejin.ch04.MathTest;
 
 public class Main {
 
     public static void main(String[] args) {
-        Practice01.main();
+        MathTest.main();
     }
 }
---
+++ src/dev/heejin/ch04/MathTest.java
@@ -0,0 +1,11 @@
+package dev.heejin.ch04;
+
+public class MathTest {
+
+    public static void main() {
+        // 1 ~ 10 사이 정수
+        int result = (int) (Math.random() * 10) + 1;
+        System.out.println("result = " + result);
+
+    }
+}
