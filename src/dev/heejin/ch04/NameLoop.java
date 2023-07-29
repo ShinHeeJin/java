0841b73b887c2c43e50a563ee02379311127ac77
ShinHeeJin <sisesu92@gmail.com>
2023-07-29 21:33:21 +0900

    ch04 Named While Loop

--- src/dev/heejin/ch04/NameLoop.java
+++ src/dev/heejin/ch04/NameLoop.java
@@ -3,6 +3,7 @@ package dev.heejin.ch04;
 public class NameLoop {
 
     public static void main() {
+        // Named For Loop
         Loop1 : for (int i = 2; i <= 9 ; i++) {
             Loop2 : for (int j = 1; j <= 9 ; j++) {
                 if (j==5){
@@ -14,6 +15,31 @@ public class NameLoop {
                 break Loop1;
             }
             System.out.println("=====================");
+        }
+
+        // Named While Loop
+        int a = 1;
+        System.out.println("start - a = " + a);
+        outer:
+        while (true){
+            System.out.println("==================");
+            if ( a == 5 ){
+                break;
+            } else if ( a == 6 ) {
+                continue outer;
+            }
+            a++;
+
+            inner:
+            while (true) {
+                System.out.println("------------------");
+                System.out.println("a = " + a);
+                if (a == 7) {
+                    System.out.println("a is 7");
+                    break outer;
+                }
+                a++;
+            }
         }
     }
 }
