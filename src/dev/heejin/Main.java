bb0c64aecb85c36ba5778dcd130c6631900137b7
ShinHeeJin <sisesu92@gmail.com>
2023-07-30 22:14:13 +0900

    ch07 - 내부 클래스 제어자 & 접근자 3

--- src/dev/heejin/Main.java
+++ src/dev/heejin/Main.java
@@ -1,10 +1,10 @@
 package dev.heejin;
 
-import dev.heejin.ch07.InterfaceDefaultMethod;
+import dev.heejin.ch07.InnerClassPractice2;
 
 public class Main {
 
     public static void main(String[] args) {
-        new InterfaceDefaultMethod().main();
+        new InnerClassPractice2().main();
     }
 }
